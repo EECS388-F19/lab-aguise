c47c15245f140f05e42d1c3e97036ea509fde423
Andrew Guise <aguise@umich.edu>
2019-09-09 16:34:31 -0400

    completes lab assignment

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+print ("Andrew Guise")
+
+num1 = random.randint(0, 100)
+num2 = random.randint(0, 100)
+print(num1)
+print(num2)
+print("Sum =", num1+num2)
+print("Average =", (num1+num2) / 2)
